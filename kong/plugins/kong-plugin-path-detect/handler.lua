58606abf8b902a320a4b3c49c28ce2ae0b0e63fd
nduyphuong <nduyphuong@cmc.com.vn>
2022-04-02 00:17:31 +0700

    handler

--- kong/plugins/kong-plugin-path-detect/handler.lua
+++ kong/plugins/kong-plugin-path-detect/handler.lua
@@ -1,32 +1,8 @@
--- If you're not sure your plugin is executing, uncomment the line below and restart Kong
--- then it will throw an error which indicates the plugin is being loaded at least.
-
---assert(ngx.get_phase() == "timer", "The world is coming to an end!")
-
----------------------------------------------------------------------------------------------
--- In the code below, just remove the opening brackets; `[[` to enable a specific handler
---
--- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
--- on when exactly they are invoked and what limitations each handler has.
----------------------------------------------------------------------------------------------
-
-
-
 local plugin = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
   VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
 
-
-
--- do initialization here, any module level code runs in the 'init_by_lua_block',
--- before worker processes are forked. So anything you add here will run once,
--- but be available in all workers.
-
-
-
--- handles more initialization, but AFTER the worker process has been forked/created.
--- It runs in the 'init_worker_by_lua_block'
 function plugin:init_worker()
 
   -- your custom code here
@@ -64,17 +40,16 @@ end --]]
 
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
-
-  -- your custom code here
+  ngx.log(ngx.ERR, "============ Hello World! ============ 1")
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
   kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
-
+  ngx.log(ngx.ERR, "============ Hello World! ============")
 end --]]
 
 
 -- runs in the 'header_filter_by_lua_block'
 function plugin:header_filter(plugin_conf)
-
+  ngx.log(ngx.ERR, "============ Hello World! ============")
   -- your custom code here, for example;
   kong.response.set_header(plugin_conf.response_header, "this is on the response")
 end --]]
